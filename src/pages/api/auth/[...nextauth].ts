1650c8849b08d6ac958ae4fde3bb112c37ca1c88
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-03-10 21:28:04 -0300

    Ig.news

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import GithubProvider from "next-auth/providers/github";
+import nextAuth from "next-auth";
+
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+import { signIn } from "next-auth/react";
+
+export default nextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_email"), q.Casefold(user.email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
